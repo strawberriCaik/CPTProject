880c5b8e74671f03d6f2f1792487b8ea3ef98234
strawberriCaik <bosco.zhen8@gmail.com>
2024-06-09 11:20:48 -0400

    added working shop panel

--- CPT/src/BoscoZhenCPT/ShopPanel.java
+++ CPT/src/BoscoZhenCPT/ShopPanel.java
@@ -17,14 +17,13 @@ import tile.Tile;
 public class ShopPanel extends BasePanel{
 
 	JPanel pnl = new JPanel (); //JPanel for shop
-	Button buy,sell,quests,back; 
-	JLabel instruction_text;
+	Button buy,sell,quests,back, inv1; 
 	
-	JLabel name = new JLabel ( "Welcome to Joe's Italian Restaurant!" );
+	boolean state = false;
 	
 	JLabel imgLabel = new JLabel (new ImageIcon("/tiles/buyM.png"));
 	
-	public BufferedImage buyM, sellM, questsM, itemsM;
+	public BufferedImage buyM, sellM, questsM, itemsM, boyM;
 		
 	public static Image loadImage(String name, int width, int height) {
 		return new ImageIcon(name).getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH); 
@@ -41,7 +40,6 @@ public class ShopPanel extends BasePanel{
 		getImage();// calls get image method to gather images from folders
 		setFocusable(true);
 		
-		pnl.add(name);
 		pnl.add(imgLabel);
 		add(pnl);
 		pnl.setVisible(true);
@@ -55,6 +53,7 @@ public class ShopPanel extends BasePanel{
 		try {//scan images from res folder and then goes into player package and take image and create
 			buyM = ImageIO.read(getClass().getResourceAsStream("/tiles/buyM.png"));
 			itemsM = ImageIO.read(getClass().getResourceAsStream("/tiles/itemsM.png"));
+			boyM = ImageIO.read(getClass().getResource("/player/boy_down_1.png"));
 			
 		}catch(IOException e) {//catch 
 			e.printStackTrace();// handles errors
@@ -62,20 +61,35 @@ public class ShopPanel extends BasePanel{
 	}
 	
 	@Override public void paintComponent(Graphics g) {
-		g.setColor(BACKGROUND);
-		g.fillRect(0, 0, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
+//		g.setColor(BACKGROUND);
+//		g.fillRect(0, 0, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
+
+//		instruction_text.setVisible(false);
+		Graphics2D g2 = (Graphics2D)g;// convert Graphics g into Graphics 2D
+		
+		g2.setColor(getBackground());
+		g2.fillRect(0, 0, getWidth(), getHeight());
+//		g2.drawImage(buyM,350,50,500,500,null);//print out image buyM onto the shop panel 
+		g2.drawImage(itemsM,350,550,500,65,null);
+		if (state) {
+			g2.drawImage(buyM, 350, 50, 500, 500, this);
+			// print out image buyM onto the shop panel
+		} else {
+			g2.drawImage(boyM, 350, 50, 500, 500, this);
+			// print out image buyM onto theshop panel
+		}
+		
+		inv1 = new Button (""+GamePanel.inventory[0], 420,570,20,20);
+//		instruction_text = new JLabel("<html>"
+//				   + GamePanel.inventory[0]+" ");
+//		add(instruction_text);
+//		instruction_text.setBounds(425, 480, 400, 200);	
 		buy.draw(g);
 		sell.draw(g);
 		quests.draw(g);	
 		back.draw(g);
+		inv1.draw(g);
 		
-		instruction_text = new JLabel("<html>"
-				   + GamePanel.inventory[0]+" ");
-				add(instruction_text);
-		instruction_text.setBounds(425, 480, 400, 200);	
-		Graphics2D g2 = (Graphics2D)g;// convert Graphics g into Graphics 2D
-		g2.drawImage(buyM,350,50,500,500,null);//print out image buyM onto the shop panel 
-		g2.drawImage(itemsM,350,550,500,65,null);
 		
 	}
 	
@@ -83,11 +97,18 @@ public class ShopPanel extends BasePanel{
 	@Override public void mouseReleased(MouseEvent e) {
 		if(activeButton == buy) {
 			GamePanel.inventory[0]++;
-			remove(instruction_text);
-			repaint();
+			state=true;
+            Main.navigation.show(Main.centerPanel, "Game");
+            Main.navigation.show(Main.centerPanel, "Shop");
+        	//pnl.repaint();
+//			remove(instruction_text);
+		
 		}
 		if(activeButton == sell) {
-			
+			state=false;
+            Main.navigation.show(Main.centerPanel, "Game");
+            Main.navigation.show(Main.centerPanel, "Shop");
+        	//pnl.repaint();
 		}
 		if(activeButton == quests) {
 			
